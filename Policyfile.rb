eb6cdf6935326e29adcf0c36873686cae6a4ddfa
mauroseb <mauro.oddi@gmail.com>
2024-10-13 16:10:58 +0200

    First commit.
    Signed-off-by: mauroseb <mauro.oddi@gmail.com>

--- Policyfile.rb
+++ Policyfile.rb
@@ -10,7 +10,7 @@ name 'ice_motd'
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'ice_motd::default'
+run_list 'ice_motd'
 
 # Specify a custom source for a single cookbook:
 cookbook 'ice_motd', path: '.'
